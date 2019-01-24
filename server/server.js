72e020bc128c925478b5258d743cc82aa36b09f7
Jens Torgeir Solaugsten <torgeir@solaugsten.no>
2019-01-24 10:21:56 +0100

    Server listen on localhost:3000

--- server/server.js
+++ server/server.js
@@ -5,4 +5,6 @@ app.get('/', function(req, res){
   res.send('Test implementation');
 });
 
-app.listen(3000);
+app.listen(3000, 'localhost', function() {
+  console.log("paexdlistapp started on port %d in %s mode", app.address().port, app.settings.env);
+});

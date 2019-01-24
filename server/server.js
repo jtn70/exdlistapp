79eea504c516394d373ba76fd0fb71e153218859
Jens Torgeir Solaugsten <torgeir@solaugsten.no>
2019-01-24 12:18:13 +0100

    Startet dynamic list implementation.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 var express = require('express');
-var http = require('http');
+var http    = require('http');
+var mysql   = require('mysql');
 
 var app = express();
 var server = http.createServer(app);
+var dbcon = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'N0nD3b311icata',
+  database: 'paexdlistapp'
+});
+
+app.get('/eksamenEDL.txt', function(req, res){
+  res.send('eksamenEDL.txt');
+});
 
 app.get('/', function(req, res){
   res.send('Test implementation');

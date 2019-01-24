7d25a4880916132b0c968a471745a6a5a8d8b9e6
Jens Torgeir Solaugsten <torgeir@solaugsten.no>
2019-01-24 10:45:15 +0100

    Remove errors.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var http = require('http');
+
 var app = express();
+var server = http.createServer(app);
 
 app.get('/', function(req, res){
   res.send('Test implementation');
 });
 
-app.listen(3000, 'localhost', function() {
-  console.log("paexdlistapp started on port %d in %s mode", app.address().port, app.settings.env);
+server.listen(3000, 'localhost');
+server.on('listening', function() {
+  console.log('paexdlistapp server started on port %s at %s', server.address().port, server.address().address);
 });
